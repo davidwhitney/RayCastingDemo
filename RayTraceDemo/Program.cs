7217d2aef044edd90c4d552c3a4ce060ca811ce2
David Whitney <david@davidwhitney.co.uk>
2020-02-09 22:14:55 +0000

    Trying to hack diagnostics in by tracking the path travelled.

--- RayTraceDemo/Program.cs
+++ RayTraceDemo/Program.cs
@@ -13,47 +13,50 @@ namespace RayTraceDemo
         static void Main(string[] args)
         {
             var world = new Map(new [] {
-                "####################",
-                "#                  #",
-                "#      ######      #",
-                "#         ###      #",
-                "###       ###      #",
-                "###                #",
-                "###                #",
-                "###                #",
-                "###         c      #",
-                "####################",
+                "########################################",
+                "#                                      #",
+                "#      #########                       #",
+                "#         ###                          #",
+                "###       ###                          #",
+                "###                                    #",
+                "##                                     #",
+                "#                                      #",
+                "#                                      #",
+                "#            c                         #",
+                "###                                    #",
+                "###                                    #",
+                "########################################",
             });
 
             const int renderWidth = 2560;
             const int renderHeight = 1440;
 
             var camera = new Camera(world.CameraLocation.X, world.CameraLocation.Y, world) { DirectionInDegrees = 180 };
-            var rayContacts = new List<Intersection>();
+            var rayContacts = new List<PathNode>();
 
-            for (var column = 0.0; column < renderWidth; column++)
+            for (var column = 0; column < renderWidth; column++)
             {
-                var x = column / renderWidth - 0.5;
+                var x = (double) column / renderWidth - 0.5;
                 var angle = Math.Atan2(x, camera.FocalLength);
-                var ray = camera.SetDirection(angle).Ray().ToList();
-                rayContacts.Add(ray.Last());
+                var rayPath = camera.SetDirection(angle).Ray();
+                rayContacts.Add(rayPath.Last());
             }
-            
+
             DrawToImage(renderHeight, renderWidth, rayContacts, camera);
 
             Console.Write("\r\n");
             Console.ReadKey();
         }
 
-        private static void DrawToImage(int renderHeight, int renderWidth, IReadOnlyList<Intersection> rayContacts, Camera camera)
+        private static void DrawToImage(int renderHeight, int renderWidth, IReadOnlyList<PathNode> rayContacts, Camera camera)
         {
-            var pixels = new Rgba32[renderHeight, renderWidth];
+            var pixels = new Rgba32[renderWidth, renderHeight];
 
             for (var x = 0; x < rayContacts.Count; x++)
             {
                 var ray = rayContacts[x];
 
-                var thisHeight = (renderHeight * ray.Height) / (ray.Distance / 3);
+                var thisHeight = (renderHeight * ray.Height) / (ray.Distance / 2.5);
                 thisHeight = ray.Height <= 0 ? 0 : thisHeight;
                 
                 var texture = SelectTexture(ray, camera);
@@ -67,10 +70,9 @@ namespace RayTraceDemo
                 {
                     var index = renderHeight - y - 1;
                     index = index < 0 ? 0 : index;
-
                     index -= offset;
 
-                    pixels[index, x] = texture;
+                    pixels[x, index] = texture;
                 }
             }
             
@@ -80,7 +82,7 @@ namespace RayTraceDemo
             {
                 for (var x = 0; x < renderWidth; x++)
                 {
-                    img[x, y] = pixels[y, x];
+                    img[x, y] = pixels[x, y];
                 }
             }
 
@@ -93,10 +95,10 @@ namespace RayTraceDemo
             Process.Start(processStartInfo);
         }
 
-        private static Rgba32 SelectTexture(Intersection ray, Camera camera)
+        private static Rgba32 SelectTexture(PathNode ray, Camera camera)
         {
             var percentage = (ray.Distance / camera.Range) * 100;
-            var brightness = 255 - ((255 / 100) * percentage);
+            var brightness = 255 - ((255.00 / 100) * percentage);
             return new Rgba32((byte)brightness, (byte)brightness, (byte)brightness);
         }
     }
@@ -130,7 +132,7 @@ namespace RayTraceDemo
 
         public Map World { get; }
 
-        public Camera(double x, double y, Map world, int range = 14, double focalLength = 0.8)
+        public Camera(double x, double y, Map world, int range = 25, double focalLength = 0.8)
         {
             Location2D = new Location2D { X = x, Y = y };
             World = world;
@@ -140,45 +142,56 @@ namespace RayTraceDemo
 
         public Camera SetDirection(double angle)
         {
-            CurrentDirection = new CastDirection(DirectionInDegrees + angle);
+            var directionInDegrees = DirectionInDegrees + angle;
+            CurrentDirection = new CastDirection(directionInDegrees);
             return this;
         }
 
-        public IEnumerable<Intersection> Ray() => Ray(new Intersection(Location2D));
+        public List<PathNode> Ray() => Ray(new PathNode(Location2D));
 
-        public IEnumerable<Intersection> Ray(Intersection origin)
+        public List<PathNode> Ray(PathNode origin)
         {
-            var stepX = ComputeNextStepLocation(CurrentDirection.Sin, CurrentDirection.Cos, origin.Location.X, origin.Location.Y);
-            var stepY = ComputeNextStepLocation(CurrentDirection.Cos, CurrentDirection.Sin, origin.Location.Y, origin.Location.X, true);
-            
-            var nextStep = stepX.Length2 < stepY.Length2
-                ? Inspect(stepX, 1, 0, origin.Distance, stepX.Location.Y)
-                : Inspect(stepY, 0, 1, origin.Distance, stepY.Location.X);
+            var rayPath = new List<PathNode>();
+            var currentStep = origin;
 
-            if (nextStep.Distance > Range)
+            while (true)
             {
-                return new List<Intersection> {origin};
-            }
+                rayPath.Add(currentStep);
 
-            if (nextStep.Height > 0)
-            {
-                return new List<Intersection> { nextStep };
-            }
+                var stepX = ComputeNextStepLocation(CurrentDirection.Sin, CurrentDirection.Cos, currentStep.Location.X, currentStep.Location.Y);
+                var stepY = ComputeNextStepLocation(CurrentDirection.Cos, CurrentDirection.Sin, currentStep.Location.Y, currentStep.Location.X, true);
 
-            return new List<Intersection> {origin}.Concat(Ray(nextStep));
+                var nextStep = stepX.Length2 < stepY.Length2
+                    ? Inspect(stepX, 1, 0, currentStep.Distance, stepX.Location.Y)
+                    : Inspect(stepY, 0, 1, currentStep.Distance, stepY.Location.X);
+
+                if (nextStep.Distance > Range)
+                {
+                    return rayPath;
+                }
+
+                if (nextStep.IsNotAVerticalSurface)
+                {
+                    currentStep = nextStep;
+                    continue;
+                }
+
+                rayPath.Add(nextStep);
+                return rayPath;
+            }
         }
 
-        public Intersection ComputeNextStepLocation(double rise, double run, double x, double y, bool inverted = false)
+        public PathNode ComputeNextStepLocation(double rise, double run, double x, double y, bool inverted = false)
         {
             if (run == 0.0)
             {
-                return Intersection.NoWall;
+                return PathNode.NoWall;
             }
 
             var dx = run > 0 ? Math.Floor(x + 1) - x : Math.Ceiling(x - 1) - x;
             var dy = dx * (rise / run);
 
-            return new Intersection
+            return new PathNode
             {
                 Location = new Location2D 
                 {
@@ -189,7 +202,7 @@ namespace RayTraceDemo
             };
         }
 
-        public Intersection Inspect(Intersection step, int shiftX, int shiftY, double distance, double offset)
+        public PathNode Inspect(PathNode step, int shiftX, int shiftY, double distance, double offset)
         {
             var dx = CurrentDirection.Cos < 0 ? shiftX : 0;
             var dy = CurrentDirection.Sin < 0 ? shiftY : 0;
@@ -227,7 +240,7 @@ namespace RayTraceDemo
         }
     }
 
-    public class Intersection
+    public class PathNode
     {
         public Location2D Location { get; set; }
         public double Length2 { get; set; }
@@ -236,11 +249,11 @@ namespace RayTraceDemo
 
         public double Offset { get; set; }
 
-        public Intersection()
+        public PathNode()
         {
         }
 
-        public Intersection(Location2D location2D, int length2 = 0, int height = 0, int distance = 0)
+        public PathNode(Location2D location2D, int length2 = 0, int height = 0, int distance = 0)
         {
             Location = location2D;
             Length2 = length2;
@@ -248,7 +261,9 @@ namespace RayTraceDemo
             Distance = distance;
         }
 
-        public static Intersection NoWall { get; } = new Intersection(new Location2D());
+        public static PathNode NoWall { get; } = new PathNode(new Location2D());
+
+        public bool IsNotAVerticalSurface => Height <= 0;
     }
     
     public struct Location2D
